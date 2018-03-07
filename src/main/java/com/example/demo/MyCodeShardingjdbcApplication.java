4f9c2a32bc99319330754005461a6543031b94ab
jianlucky <jianlucky@192.168.19.110>
2018-03-07 15:43:02 +0800

    create

---
+++ src/main/java/com/example/demo/MyCodeShardingjdbcApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCodeShardingjdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyCodeShardingjdbcApplication.class, args);
+	}
+}
